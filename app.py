7a4fefc9c98620ce984b36435cc579212deb2d65
Aidan Daffa <85110921+aidandf29@users.noreply.github.com>
2026-09-15 09:47:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -304,8 +304,8 @@ with st.sidebar:
     st.button(
         "Sinkronisasi data dengan HRIS & KATALIS",
         icon=":material/sync:",
-        use_container_width=True,
-        # disabled=True,
+        # use_container_width=True,
+        disabled=false,
         help="Integrasi belum tersedia — memerlukan koneksi API HRIS dan KATALIS BI.",
     )
 
